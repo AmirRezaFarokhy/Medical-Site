fdd3692ec9765934d01d09090418d8398e0bd19f
AmirRezaFarokhy <amirreza.farokhy81@gmail.com>
2023-07-02 15:34:55 +0330

    add css files

--- Django_Web/settings.py
+++ Django_Web/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' #with or without slash at beginning	
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS=[
+#     "siteornitho/static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
