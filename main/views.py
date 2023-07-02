2bcce6352a38027730af941c4ad6b208160ef6ae
AmirRezaFarokhy <amirreza.farokhy81@gmail.com>
2023-07-02 16:13:44 +0330

    change view.py

--- main/views.py
+++ main/views.py
@@ -19,20 +19,12 @@ def validateCustomer(customer):
         error_message = "Please Enter your First Name !!"
     elif len(customer.name) < 3:
         error_message = 'First Name must be 3 char long or more'
-    elif not customer.lastname:
+    elif not customer.last_name:
         error_message = 'Please Enter your Last Name'
-    elif len(customer.lastname) < 3:
+    elif len(customer.last_name) < 3:
         error_message = 'Last Name must be 3 char long or more'
-    elif not customer.phone:
-        error_message = 'Enter your Phone Number'
-    elif len(customer.phone) < 10:
-        error_message = 'Phone Number must be 10 char Long'
-    elif len(customer.password) < 5:
-        error_message = 'Password must be 5 char long'
     elif len(customer.email) < 5:
         error_message = 'Email must be 5 char long'
-    elif customer.ifExist():
-        error_message = 'Email Address Already Registered..'
 
     return error_message
 
@@ -101,7 +93,7 @@ class Signup(View):
         if not error_message:
             print(first_name, last_name, phone, email, password)
             customer.password = make_password(customer.password)
-            customer.register()
+            customer.save()
             return redirect('main:homepage')
         else:
             data = {
@@ -163,7 +155,7 @@ def homepage(request):
 
     if not error_message:
         print(values)
-        patient.register()
+        patient.save()
         return redirect('main:homepage')
 
     return render(request, 
