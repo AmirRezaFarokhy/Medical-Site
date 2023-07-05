7a9441ad38a3398abec67c32ecbafe72df8d2cb1
AmirRezaFarokhy <amirreza.farokhy81@gmail.com>
2023-07-05 19:49:42 +0330

    changes templates file and App.py

--- FastAPI_Web/App.py
+++ FastAPI_Web/App.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Request 
+from fastapi import FastAPI, status, Request, HTTPException 
 from fastapi.staticfiles import StaticFiles 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
@@ -9,8 +9,7 @@ from starlette.middleware.sessions import SessionMiddleware
 
 
 app = FastAPI()
-app.mount('/static', StaticFiles(directory='static'), 
-          name='static')
+app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
 
@@ -22,20 +21,29 @@ async def some_middleware(request: Request, call_next):
     response = await call_next(request)
     session = request.cookies.get('session')
     if session:
-        response.set_cookie(key='session', value=request.cookies.get('session'), httponly=True)
+        response.set_cookie(key='session', 
+                            value=request.cookies.get('session'), 
+                            httponly=True)
     return response
 
 
 @app.get('/', status_code=status.HTTP_200_OK, response_class=HTMLResponse)
 async def homepage(request:Request):
-    try:
+    return templates.TemplateResponse('home.html', 
+                            {'request':request, 
+                            'data':None})
+
+
+@app.post('/', status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
+async def homepage(request:Request):
+    if request.session['doctor'] is not None:
         conn = pg.connect(
-            host='localhost',
-            port=5432,
-            dbname='postgres',
-            password='postgres',
-            user='postgres'
-        )
+                    host='localhost',
+                    port=5432,
+                    dbname='postgres',
+                    password='postgres',
+                    user='postgres'
+                )
         print("Connected succesfully...")
 
         sql_query = '''INSERT INTO main_patientprofile(
@@ -48,46 +56,15 @@ async def homepage(request:Request):
                     VALUES (%s, %s, %s, %s, %s, %s)    
                 '''
 
-    except Exception as e:
-        print(f"Can't Connect for {e}")
 
-
-    return templates.TemplateResponse('home.html', 
-                                {'request':request, 
-                                'data':None})
-
-
-@app.get('/login', status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
+@app.get('/login', status_code=status.HTTP_200_OK, response_class=HTMLResponse)
 async def Login(request:Request):
-    conn = pg.connect(
-        host='localhost',
-        port=5432,
-        dbname='postgres',
-        password='postgres',
-        user='postgres'
-    )
-    print("Connected succesfully...")
-
-    conn.autocommit = True 
-    cur = conn.cursor()
-    
-    # name = request.POST.get('doctor')
-    # password = request.POST.get('password')
-    # # index 1 show tha name of doctor and index -1 or 3 show password
-    # # we must check the password id correct or not
-    # sql_query = "SELECT * FROM main_doctor"
-    # cur.execute(sql_query)
-    # for doctor_row in cur.fetchall():
-    #     if doctor_row[1]==name and doctor_row[-1]==password:
-    #         request.session['doctro'] = doctor_row[0] # that's ID of doctor
-    #         # return RedirectResponse('home.html')
-
     return templates.TemplateResponse('login.html', 
                             {'request':request, 
                             'data':None})
 
 
-@app.get('/signup', status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
+@app.post('/login', status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
 async def Login(request:Request):
     conn = pg.connect(
         host='localhost',
@@ -100,18 +77,36 @@ async def Login(request:Request):
 
     conn.autocommit = True 
     cur = conn.cursor()
-    
-    # name = request.POST.get('doctor')
-    # password = request.POST.get('password')
-    # # index 1 show tha name of doctor and index -1 or 3 show password
-    # # we must check the password id correct or not
-    # sql_query = "SELECT * FROM main_doctor"
-    # cur.execute(sql_query)
-    # for doctor_row in cur.fetchall():
-    #     if doctor_row[1]==name and doctor_row[-1]==password:
-    #         request.session['doctro'] = doctor_row[0] # that's ID of doctor
-    #         # return RedirectResponse('home.html')
 
+    form = await request.form()
+    form = dict(form)
+    print(form)
+    name = form["doctor"]
+    password = form['password']
+    # index 1 show tha name of doctor and index -1 or 3 show password
+    # we must check the password id correct or not
+    sql_query = "SELECT * FROM main_doctor"
+    cur.execute(sql_query)
+    for doctor_row in cur.fetchall():
+        if doctor_row[1]==name and doctor_row[-1]==password:
+            request.session['doctor'] = doctor_row[0] # that's ID of doctor
+            return templates.TemplateResponse('home.html', 
+                            {'request':request, 
+                            'data':doctor_row[1]})
+        else:
+            return templates.TemplateResponse('login.html', 
+                            {'request':request, 
+                            'data':"Invalid Profile!!!"})
+    
+
+@app.get('/logout', response_class=HTMLResponse)
+async def Logout(request:Request):
+    request.session['doctor'] = None 
+    return RedirectResponse(url='/')
+
+
+@app.get('/signup', status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
+async def Login(request:Request):
     return templates.TemplateResponse('signup.html', 
                             {'request':request, 
                             'data':None})
